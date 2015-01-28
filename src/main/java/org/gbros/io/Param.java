ed02ed7c231d95721cab8dfdae33b6777f6a49cf
裴龙武 <peilw625311@gmail.com>
2015-01-28 17:01:42 +0800

    param class change

--- src/main/java/org/gbros/io/Param.java
+++ src/main/java/org/gbros/io/Param.java
@@ -1,16 +1,40 @@
 package org.gbros.io;
 
+import org.gbros.utils.StringKit;
+
 public class Param {
 
   private String name;
   private String value;
+  private String defaultValue = "";
   
   public Param(){
   }
   
+  /**
+   * param constructor.
+   * @param name
+   * @param value
+   */
   public Param(String name, String value) {
     this.name = name;
-    this.value = value;
+    if (StringKit.isBlank(value)) {
+      this.value = defaultValue;
+    }
+  }
+  
+  /**
+   * param constructor.
+   * @param name
+   * @param value
+   * @param defaultValue
+   */
+  public Param(String name, String value, String defaultValue) {
+    this.name = name;
+    this.defaultValue = defaultValue;
+    if (StringKit.isBlank(value)) {
+      this.value = defaultValue;
+    }
   }
 
   public String getName() {
@@ -28,6 +52,13 @@ public class Param {
   public void setValue(String value) {
     this.value = value;
   }
-  
+
+  public String getDefaultValue() {
+    return defaultValue;
+  }
+
+  public void setDefaultValue(String defaultValue) {
+    this.defaultValue = defaultValue;
+  }
   
 }
--- src/test/java/org/gbros/test/MyResourceTest.java
+++ src/test/java/org/gbros/test/MyResourceTest.java
@@ -14,37 +14,38 @@ import static org.junit.Assert.assertEquals;
 
 public class MyResourceTest {
 
-    private HttpServer server;
-    private WebTarget target;
+  private HttpServer server;
+  private WebTarget target;
 
-    @Before
-    public void setUp() throws Exception {
-        // start the server
-        server = Main.startServer();
-        // create the client
-        Client c = ClientBuilder.newClient();
+  @Before
+  public void setUp() throws Exception {
+    // start the server
+    server = Main.startServer();
+    // create the client
+    Client c = ClientBuilder.newClient();
 
-        // uncomment the following line if you want to enable
-        // support for JSON in the client (you also have to uncomment
-        // dependency on jersey-media-json module in pom.xml and Main.startServer())
-        // --
-        // c.configuration().enable(new org.glassfish.jersey.media.json.JsonJaxbFeature());
+    // uncomment the following line if you want to enable
+    // support for JSON in the client (you also have to uncomment
+    // dependency on jersey-media-json module in pom.xml and Main.startServer())
+    // --
+    // c.configuration().enable(new
+    // org.glassfish.jersey.media.json.JsonJaxbFeature());
 
-        target = c.target(Main.BASE_URI);
-    }
+    target = c.target(Main.BASE_URI);
+  }
 
-    @After
-    public void tearDown() throws Exception {
-        server.shutdown();
-    }
+  @After
+  public void tearDown() throws Exception {
+    server.shutdown();
+  }
 
-    /**
-     * Test to see that the message "Got it!" is sent in the response.
-     */
-    @Test
-    public void testGetIt() {
-        String responseMsg = target.path("myresource").request().get(String.class);
-        System.out.println(responseMsg);
-        assertEquals("Got it!", responseMsg);
-    }
+  /**
+   * Test to see that the message "Got it!" is sent in the response.
+   */
+  @Test
+  public void testGetIt() {
+    String responseMsg = target.path("myresource").request().get(String.class);
+    System.out.println(responseMsg);
+    assertEquals("Got it!", responseMsg);
+  }
 }
